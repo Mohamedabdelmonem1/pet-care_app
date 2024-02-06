64e19b1b9ea3d2d32e6837d21cfd8f45755830fa
Elaf <mm345694@gmail.com>
2024-02-06 16:31:17 +0200

    updates

--- lib/pages/shop/carts.dart
+++ lib/pages/shop/carts.dart
@@ -3,7 +3,6 @@ import 'package:petcare_app/helper/end_points.dart';
 import 'package:petcare_app/helper/remote/dio_helper.dart';
 import 'package:petcare_app/helper/snack_helper.dart';
 import 'package:petcare_app/models/cart_item.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -17,6 +16,7 @@ class cart extends StatefulWidget {
 class _cartState extends State<cart> {
   @override
   void initState() {
+    super.initState();
     // TODO: implement initState
     getMyCart();
   }
@@ -27,139 +27,141 @@ class _cartState extends State<cart> {
     TextEditingController namecard = TextEditingController();
 
     return SafeArea(
-        child: Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            icon: Icon(
-              Icons.arrow_back,
-              color: Colors.black,
-            )),
-        backgroundColor: Colors.white,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30))),
-        title: Text(
-          "pet life",
-          style: TextStyle(
-              fontSize: 27,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (c) => cart()));
-            },
-            icon: Icon(
-              Icons.shopping_bag_outlined,
-              color: Colors.green,
-              size: 30,
-            ),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              )),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30))),
+          title: Text(
+            "pet life",
+            style: TextStyle(
+                fontSize: 27,
+                fontWeight: FontWeight.bold,
+                color: Colors.blueAccent),
           ),
-          SizedBox(
-            width: 15,
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Color(0xff269A41),
-        onPressed: () {
-          showModalBottomSheet(
-              context: context,
-              builder: (context) {
-                return SingleChildScrollView(
-                  child: Container(
-                    height: 240,
-                    child: Column(children: [
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                            border: Border.all(width: 1)),
-                        height: 60,
-                        margin: EdgeInsets.only(
-                            top: 20, bottom: 10, right: 25, left: 25),
-                        child: TextFormField(
-                          keyboardType: TextInputType.number,
-                          controller: cardnumber,
-                          decoration: InputDecoration(
-                            labelText: "card number",
-                            labelStyle: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                            filled: true,
-                            fillColor: Colors.grey[1],
-                            suffixText: "VISA",
-                            suffixStyle: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                      Container(
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
-                            border: Border.all(width: 1)),
-                        height: 60,
-                        margin:
-                            EdgeInsets.only(bottom: 20, right: 25, left: 25),
-                        child: TextFormField(
-                          controller: namecard,
-                          decoration: InputDecoration(
-                            labelText: "name on card",
-                            labelStyle: TextStyle(
-                                color: Colors.blue,
-                                fontSize: 15,
-                                fontWeight: FontWeight.bold),
-                            filled: true,
-                            fillColor: Colors.grey[1],
-                          ),
-                        ),
-                      ),
-                      Container(
-                        height: 40.h,
-                        width: 220,
-                        color: Color(0xff075965),
-                        child: MaterialButton(
-                          onPressed: () {
-                            confirm();
-                          },
-                          child: Center(
-                              child: Text(
-                            "Place Order",
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          )),
-                        ),
-                      )
-                    ]),
-                  ),
-                );
-              });
-        },
-        child: Icon(Icons.landslide_sharp),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 10,
+          actions: [
+            IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (c) => cart()));
+              },
+              icon: Icon(
+                Icons.shopping_bag_outlined,
+                color: Colors.green,
+                size: 30,
               ),
-              ListView.separated(
-                physics: BouncingScrollPhysics(),
-                shrinkWrap: true,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
+            ),
+            SizedBox(
+              width: 15,
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color(0xff269A41),
+          onPressed: () {
+            showModalBottomSheet(
+                context: context,
+                builder: (context) {
+                  return SingleChildScrollView(
+                    child: Container(
+                      height: 240,
+                      child: Column(children: [
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              border: Border.all(width: 1)),
+                          height: 60,
+                          margin: EdgeInsets.only(
+                              top: 20, bottom: 10, right: 25, left: 25),
+                          child: TextFormField(
+                            keyboardType: TextInputType.number,
+                            controller: cardnumber,
+                            decoration: InputDecoration(
+                              labelText: "card number",
+                              labelStyle: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                              filled: true,
+                              fillColor: Colors.grey[1],
+                              suffixText: "VISA",
+                              suffixStyle: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(5)),
+                              border: Border.all(width: 1)),
+                          height: 60,
+                          margin:
+                              EdgeInsets.only(bottom: 20, right: 25, left: 25),
+                          child: TextFormField(
+                            controller: namecard,
+                            decoration: InputDecoration(
+                              labelText: "name on card",
+                              labelStyle: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold),
+                              filled: true,
+                              fillColor: Colors.grey[1],
+                            ),
+                          ),
+                        ),
+                        Container(
+                          height: 40.h,
+                          width: 220,
+                          color: Color(0xff075965),
+                          child: MaterialButton(
+                            onPressed: () {
+                              confirm();
+                            },
+                            child: Center(
+                                child: Text(
+                              "Place Order",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                          ),
+                        )
+                      ]),
+                    ),
+                  );
+                });
+          },
+          child: Icon(Icons.landslide_sharp),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 10,
+                ),
+                ListView.separated(
+                  physics: BouncingScrollPhysics(),
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
                       width: 300,
                       height: 300,
                       decoration: BoxDecoration(
@@ -271,12 +273,12 @@ class _cartState extends State<cart> {
                                           myCart[index].quantity =
                                               myCart[index].quantity! + 1;
                                         }
-                                        var x = (myCart[index]
-                                                .product!
-                                                .price
-                                                .toInt +
-                                            myCart[index].quantity!.toInt());
-                                        print(x);
+                                        // var x = (myCart[index]
+                                        //         .product!
+                                        //         .price
+                                        //         .toInt +
+                                        //     myCart[index].quantity!.toInt());
+                                        // print(x);
                                       });
                                     },
                                     icon: Icon(
@@ -284,7 +286,7 @@ class _cartState extends State<cart> {
                                       size: 20.h,
                                     ),
                                     color: Color(0xff269A41),
-                                  )
+                                  ),
                                 ],
                               ),
                               SizedBox(
@@ -333,27 +335,28 @@ class _cartState extends State<cart> {
                             ],
                           ),
                         ],
-                      )),
+                      ),
+                    ),
+                  ),
+                  itemCount: myCart.length,
+                  separatorBuilder: (context, index) => SizedBox(
+                    height: 5.h,
+                  ),
                 ),
-                itemCount: myCart.length,
-                separatorBuilder: (context, index) => SizedBox(
+                SizedBox(
                   height: 5.h,
                 ),
-              ),
-              SizedBox(
-                height: 5.h,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Container(
-                  width: 380.w,
-                  height: 100.h,
-                  decoration: BoxDecoration(
-                      color: Colors.grey,
-                      border: Border.all(color: Colors.black, width: 4)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    width: 380.w,
+                    height: 100.h,
+                    decoration: BoxDecoration(
+                        color: Colors.grey,
+                        border: Border.all(color: Colors.black, width: 4)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
@@ -367,7 +370,7 @@ class _cartState extends State<cart> {
                                         fontSize: 30,
                                         color: Colors.white,
                                         fontWeight: FontWeight.bold),
-                                  )
+                                  ),
                                 ],
                               ),
                             ],
@@ -406,16 +409,18 @@ class _cartState extends State<cart> {
                               ],
                             ),
                           )
-                        ]),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-              )
-              // show_price(price: 1000, count: myCart.length),
-            ],
+                // show_price(price: 1000, count: myCart.length),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 
   List<CartItem> myCart = [];
@@ -435,7 +440,6 @@ class _cartState extends State<cart> {
         myCart.add(CartItem.fromJson(e));
         setState(() {
           int curPrice = int.parse(e['product']['price']);
-          // int curitem= int.parse(e['quantity']);
           totalPrice += curPrice;
         });
       });
@@ -465,29 +469,79 @@ class _cartState extends State<cart> {
     });
   }
 
-  increase(quantity, cart_id) async {
+  increase(quantity, cartId) async {
     await DioHelper.putData(
       url: AppEndPoints.increase,
       data: {
-        "quantity": quantity,
-        "cart_id": cart_id,
+        "quantity": quantity + 1,
+        "cart_id": cartId,
       },
       token: globalUser!.token,
     ).then((value) {
-      print('success');
+      setState(() {
+        // Update the quantity in the local cart list
+        myCart.firstWhere((item) => item.id == cartId).quantity = quantity + 1;
+
+        // Recalculate the total price based on the updated quantities
+        totalPrice = calculateTotalPrice() as int;
+      });
     }).catchError((err) {
       print('error');
     });
   }
 
-  decrease(quantity, cart_id) async {
+  decrease(quantity, cartId) async {
     await DioHelper.putData(
       url: AppEndPoints.decrease,
       data: {
-        "quantity": quantity,
-        "cart_id": cart_id,
+        "quantity": quantity - 1,
+        "cart_id": cartId,
       },
       token: globalUser!.token,
-    ).then((value) {}).catchError((err) {});
+    ).then((value) {
+      setState(() {
+        // Update the quantity in the local cart list
+        myCart.firstWhere((item) => item.id == cartId).quantity = quantity - 1;
+
+        // Recalculate the total price based on the updated quantities
+        totalPrice = calculateTotalPrice() as int;
+      });
+    }).catchError((err) {
+      print('error');
+    });
   }
+
+  double calculateTotalPrice() {
+    double total = 0;
+    for (var cartItem in myCart) {
+      total += cartItem.product!.price * cartItem.quantity!;
+    }
+    return total;
+  }
+
+  // increase(quantity, cart_id) async {
+  //   await DioHelper.putData(
+  //     url: AppEndPoints.increase,
+  //     data: {
+  //       "quantity": quantity ,
+  //       "cart_id": cart_id,
+  //     },
+  //     token: globalUser!.token,
+  //   ).then((value) {
+  //     print('success');
+  //   }).catchError((err) {
+  //     print('error');
+  //   });
+  // }
+
+  // decrease(quantity, cart_id) async {
+  //   await DioHelper.putData(
+  //     url: AppEndPoints.decrease,
+  //     data: {
+  //       "quantity": quantity,
+  //       "cart_id": cart_id,
+  //     },
+  //     token: globalUser!.token,
+  //   ).then((value) {}).catchError((err) {});
+  // }
 }
